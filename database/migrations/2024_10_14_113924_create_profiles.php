9173cc96397459fbb5ceb00ee6368e10fe6afa37
sium <siumcse@gmail.com>
2024-10-14 18:38:43 +0600

    first commit

---
+++ database/migrations/2024_10_14_113924_create_profiles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('mobile', 15);
+            $table->string('city', 50);
+            $table->string('shippingAddress', 500);
+            $table->string('user_email', 50)->unique();
+            $table->foreign('usre_email')->references('email')->on('users')
+                ->restrictOnDelete()->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
